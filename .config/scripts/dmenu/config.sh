f5d43417d5e670707823795a4223430eee31d0c1
Gabriel Sebastian <gabriel777sh@gmail.com>
2022-10-03 11:42:51 +0700

    EDIT: theme color for terminal & dmenu

--- .config/scripts/dmenu/config.sh
+++ .config/scripts/dmenu/config.sh
@@ -1,11 +1,11 @@
 source $HOME/.alias_envs
 
-DMENU_FONT="JetBrainsMono Nerd Font:style=bold:size=12"
+DMENU_FONT="Iosevka Nerd Font:style=bold:size=12"
 
 # Not selected items
 DMENU_BG_NORMAL="#222222"
 DMENU_FG_NORMAL="#eeeeee"
 
 # selected item
-DMENU_BG_SELECTED="#114078"
+DMENU_BG_SELECTED="#416693"
 DMENU_FG_SELECTED="#dddddd"
